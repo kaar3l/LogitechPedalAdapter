66c791e51c55b53cbcbcab7bba6f72049a1f3fdf
Kaarel-Laptop <kaar3l@gmail.com>
2020-11-28 02:11:00 +0200

    Added schematic and connections

--- SimplePedalAdapter.ino
+++ SimplePedalAdapter.ino
@@ -1,8 +1,21 @@
-//Simple Logitech pedals adapter for Arduino Leonardo 
-
+//Simple Logitech pedals adapter for Arduino Leonardo
+//DB9           Arduino
+//Pin1 - GND - GND
+//Pin2 - Throttle - A2
+//Pin3 - Brake - A1
+//Pin4 - Clutch - A0
+//Pin6 - 5V
+//Pin9 - 5V
+//
+//DB9 connector plug facing away
+//    -------------------------
+//    |    \  1 2 3 4 5  /    |
+//    |     \  6 7 8 9  /     |
+//    -------------------------
+//
 #include "Joystick.h"
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
   JOYSTICK_TYPE_MULTI_AXIS, 0, 0,
   false, false, false, false, false, false,
   false, false, true, true, true);
